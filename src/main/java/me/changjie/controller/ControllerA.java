62d2171757a0ced93aa01d37b70c493b279041bc
changchangjie <cj869609213@qq.com>
2018-11-22 10:13:12 +0800

    cglib动态代理

--- src/main/java/me/changjie/controller/ControllerA.java
+++ src/main/java/me/changjie/controller/ControllerA.java
@@ -1,5 +1,11 @@
 package me.changjie.controller;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by ChangJie on 2018/10/30.
  */
 @RestController
-public class ControllerA {
+public class ControllerA implements BeanFactoryAware, ApplicationContextAware {
+
+//    @Autowired
+    private ApplicationContext applicationContext;
+
+    private BeanFactory beanFactory;
 
     @RequestMapping(value = "demo")
     public String demo(){
+
+        ControllerB controllerB = (ControllerB) applicationContext.getBean("controllerB");
+        controllerB.test();
         System.out.println("demo");
         return "hello";
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
